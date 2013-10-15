5fa362fc7ade35c2995161230f6e3afe47c5b36a
Aaron Beppu <abeppu@gmail.com>
2013-10-14 20:08:46 -0700

    cleanup to amb tests

--- test/toychest/amb_test.clj
+++ test/toychest/amb_test.clj
@@ -2,7 +2,7 @@
   (:use clojure.test toychest.amb)
   (:require [clojure.set :as set]))
 
-(deftest pytagorean-triple-test
+(deftest pythagorean-triple-test
   (testing "simple, multi-requirement problem"
            (is (= '([3 4 5])        
                   (amb-let [a (amb (range 1 10))
@@ -38,6 +38,7 @@
                             [b1 b2 b3]
                             [c1 c2 c3]]))))))
 
+// adapted from SICP
 (deftest multiple-dwelling-test
   (is (= "baker 3 cooper 2 fletcher 4 miller 5 smith 1" 
          (first (amb-let [baker (amb [1 2 3 4 5])
@@ -63,59 +64,63 @@
   ;; to do this more efficiently, we could
   ;; (a) break assignments up and 
   ;; (b) separate out each of the edges
-  (is (= 92 (count  (amb-let [[q1 q2 q3 q4 q5 q6 q7 q8 :as qs] 
-                              (amb (permutations #{1 2 3 4 5 6 7 8}))
-                              :where (every? identity (for [i (range 0 8)
-                                                     j (range (inc i) 8)] ;; only check to the right?
+  (is (= 92 (count (amb-let [[q1 q2 q3 q4 q5 q6 q7 q8 :as qs] 
+                             (amb (permutations #{1 2 3 4 5 6 7 8}))
+                             :where (every? identity 
+			                    (for [i (range 0 8)
+					          ;; only check to the right?		    
+                                            	  j (range (inc i) 8)]
                                                  (not (or (= (- j i) (- (qs j) (qs i)))
                                                           (= (- i j) (- (qs j) (qs i)))))))]
                              [qs])))))
+
+// http://en.wikipedia.org/wiki/Einstein%27s_puzzle
 (def einstein-puzzle
   (let [nationalities  #{:english :spanish :ukranian :japanese :norwegian}
         color #{:red :green :ivory :yellow :blue}
         beverages   #{:coffee :tea :milk :juice :water}
         animals  #{:dog :snail :zebra :fox :horse}
-        cigarettes  #{:old-golds :kools :chesterfields :lucky-strikes :parliaments}] 
+        cigarettes  #{:old-golds :kools :chesterfields :lucky-strikes :parliaments}
+	match (fn [a b as bs]
+	          (contains? (set (map vector as bs)) [a b]))
+        nextto (fn [a b as bs]
+	           (let [i (.indexOf as a)
+		         j (.indexOf bs b)]
+		     (or (= i (inc j)) (= j (inc i)))))]
     (amb-let [[nat1 nat2 nat3 nat4 nat5 :as nats] (amb (permutations nationalities))
              ;; The Norwegian lives in the first house.
              :where  (= :norwegian nat1)
              [col1 col2 col3 col4 col5 :as cols] (amb (permutations color))
              ;; The Englishman lives in the red house.
-             :where (and (contains? (set (map vector nats cols)) [:english :red])
+             :where (and (match :english :red nats cols)
                          ;; The green house is immediately to the right of the ivory house.
                          (let [i (.indexOf cols :green)
                                j (.indexOf cols :ivory)]
                            (= i (+ j 1)))
                          ;; The Norwegian lives next to the blue house.
-                         (let [i (.indexOf nats :norwegian)
-                               j (.indexOf cols :blue)]
-                           (or (= i (+ j 1)) (= i (- j 1)))))
+			 (nextto :norwegian :blue nats cols))
               [bev1 bev2 bev3 bev4 bev5 :as bevs] (amb (permutations beverages))
               :where (and (= bev3 :milk) ;; Milk is drunk in the middle house.
                           ;; Coffee is drunk in the green house.
-                          (contains? (set (map vector bevs cols)) [:coffee :green])
+			  (match :coffee :green bevs cols)
                           ;; The Ukrainian drinks tea.
-                          (contains? (set  (map vector bevs nats)) [:tea :ukranian])
-                           )
+			  (match :tea :ukranian bevs nats))                           
               [pet1 pet2 pet3 pet4 pet5 :as pets] (amb (permutations animals))
               ;; The Spaniard owns the dog.
-              :where (contains? (set (map vector nats pets)) [:spanish :dog])
+              :where (match :spanish :dog nats pets)
               [cig1 cig2 cig3 cig4 cig5 :as cigs] (amb (permutations cigarettes))
               :where (and             ;; The Old Gold smoker owns snails.
-                      (contains? (set (map vector cigs pets)) [:old-golds :snail])
+                      (match :old-golds :snail cigs pets)
                       ;; Kools are smoked in the yellow house.
-                      (contains? (set (map vector cols cigs)) [:yellow :kools])
+		      (match :yellow :kools cols cigs)
                       ;; The Lucky Strike smoker drinks orange juice.
-                      (contains? (set (map vector cigs bevs)) [:lucky-strikes :juice])
+		      (match :lucky-strikes :juice cigs bevs)
                       ;; The Japanese smokes Parliaments.
-                      (contains? (set (map vector cigs nats)) [:parliaments :japanese])
+		      (match :parliaments :japanese cigs nats)
                       ;; Kools are smoked in the house next to the house where the horse is kept.
+		      (nextto :kools :horse cigs pets)
                       ;; The man who smokes Chesterfields lives in the house next to the man with the fox.
-                      (let [i (.indexOf pets :horse) j (.indexOf cigs :kools)
-                            n (.indexOf pets :fox) m (.indexOf cigs :chesterfields)]
-                        (and (or (= i (+ j 1)) (= i (- j 1)))
-                             (or (= n (+ m 1)) (= n (- m 1)))))) 
-              ]
+		      (nextto :fox :chesterfields pets cigs))]
              (map (fn [[n c b p ci]]
                     {:nationality n :color c
                      :beverage b :pet p
